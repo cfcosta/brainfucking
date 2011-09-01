c041ca0d00d722a043b8b3d0371d43c70955c14e
Cainã Costa <cainan.costa@gmail.com>
2011-08-31 23:47:09 -0300

    Adding implementation for '<' and '>' instructions.

--- compiler.c
+++ compiler.c
@@ -7,17 +7,21 @@ void compile(char *code, int *pointer)
 {
     unsigned int i;
 
-    for (i = 0; i < strlen(code); i++) {
-        switch(code[i]) {
+    for (i = 0; i < strlen(code); i++)
+    {
+        switch(code[i])
+        {
             case '+': ++*pointer; break;
             case '-': --*pointer; break;
+            case '>': ++pointer; break;
+            case '<': --pointer; break;
         }
     }
 }
 
 int main (int argc, const char *argv[])
 {
-    int *p = malloc(sizeof(int));
+    int *p = calloc(30000,30000);
     *p = 0;
 
     compile("+", p);
@@ -28,6 +32,16 @@ int main (int argc, const char *argv[])
     assert(*p == 2);
     compile("--", p);
     assert(*p == 0);
+    compile(">", p);
+    assert(*++p == 0);
+    compile("<", p);
+    assert(*++p == 0);
+    compile(">+", p);
+    assert(*++p == 1);
+    compile("<+", p);
+    assert(*--p == 1);
+
+    printf("Success!\n");
 
     return 0;
 }
