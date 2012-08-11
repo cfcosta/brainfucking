86c0a53c9446fb36060eff865fe3d7fadf244207
Cainã Costa <cainan.costa@gmail.com>
2012-08-10 21:43:49 -0300

    Add more types to check

--- src/compiler.c
+++ src/compiler.c
@@ -5,7 +5,23 @@ void compile(int code)
   switch(code) {
     case PLUS:
       printf("got PLUS\n");
+      break;
     case MINUS:
       printf("got MINUS\n");
+      break;
+    case LEFT:
+      printf("got LEFT\n");
+      break;
+    case RIGHT:
+      printf("got RIGHT\n");
+      break;
+    case DOT:
+      printf("got DOT\n");
+      break;
+    case COMMA:
+      printf("got COMMA\n");
+      break;
+    default:
+      printf("dafuq is this?\n");
   }
 }
